ee5ac667822ea033fc21b517c6f63be60d9c7f99
l <hammerhead.software@gmail.com>
2026-04-11 22:43:19 +0300

    refactor: better chat table

--- supabase/migrations/20260411191726_create_chats.sql
+++ supabase/migrations/20260411191726_create_chats.sql
@@ -1,31 +1,24 @@
-create table if not exists chat_types (
-  id uuid primary key default gen_random_uuid(),
-  name varchar(255) not null,
-  description text
-);
-
-insert into chat_types (name, description) values
-('direct', 'direct chat between two users'),
-('group', 'group chat between multiple users'),
-('voice', 'voice chat');
+create type chat_type as enum ('direct', 'group', 'voice');
 
 create table if not exists chats (
   id uuid primary key default gen_random_uuid(),
-  created_at timestamp default now(),
-  type uuid references chat_types(id) not null
+  created_at timestamptz default now(),
+  type chat_type not null
 );
 
 create table if not exists chat_members (
-  chat_id uuid references chats(id),
-  user_id uuid references public.profiles(id),
+  chat_id uuid references chats(id) on delete cascade,
+  user_id uuid references public.profiles(id) on delete cascade,
+  joined_at timestamptz default now(),
   primary key (chat_id, user_id)
 );
 
 create table if not exists chat_messages (
   id uuid primary key default gen_random_uuid(),
-  chat_id uuid references chat_members(chat_id),
-  user_id uuid references chat_members(user_id),
+  chat_id uuid not null,
+  user_id uuid not null,
   message_content jsonb,
-  created_at timestamp default now(),
-  reply_to_chat_message_id uuid references chat_messages(id)
+  created_at timestamptz default now(),
+  reply_to_chat_message_id uuid references chat_messages(id) on delete set null,
+  foreign key (chat_id, user_id) references chat_members(chat_id, user_id)
 );
